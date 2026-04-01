bc904a72e13978794a82c1edd673a68a402151b6
DiorHomme <chomunodisaa68@gmail.com>
2026-04-01 22:16:09 +0200

    Create jwt.py

---
+++ app/utils/jwt.py
@@ -0,0 +1,8 @@
+import jwt
+from datetime import datetime, timedelta
+from app.config.settings import settings
+
+def create_token(data: dict):
+    payload = data.copy()
+    payload["exp"] = datetime.utcnow() + timedelta(hours=2)
+    return jwt.encode(payload, settings.JWT_SECRET, algorithm="HS256")
