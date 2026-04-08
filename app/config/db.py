9fb10a85f65b3e09bd6b5bb6d570a56f1b762fb1
DiorHomme <chomunodisaa68@gmail.com>
2026-04-09 01:51:23 +0200

    Update db.py

--- app/config/db.py
+++ app/config/db.py
@@ -1,7 +1,14 @@
 # app/config/db.py
+
 from motor.motor_asyncio import AsyncIOMotorClient
 from app.config.settings import settings
+from app.utils.logger import logger
 
-# Connect to MongoDB
+# Create MongoDB client
 client = AsyncIOMotorClient(settings.MONGO_URI)
+
+# Select database
 db = client[settings.DB_NAME]
+
+# Safe log (no secrets exposed)
+logger.info(f"Connected to {settings.DB_NAME} Database")
