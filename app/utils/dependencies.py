4d162f293899011d5422332c230fa79e3e0669a6
DiorHomme <chomunodisaa68@gmail.com>
2026-04-23 01:15:15 +0200

    Update dependencies.py

--- app/utils/dependencies.py
+++ app/utils/dependencies.py
@@ -1,6 +1,7 @@
 from fastapi import Depends, HTTPException
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 import jwt
+from jwt import DecodeError, ExpiredSignatureError, InvalidSignatureError
 from app.config.settings import settings
 
 security = HTTPBearer()
@@ -11,5 +12,9 @@ def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(securit
     try:
         payload = jwt.decode(token, settings.JWT_SECRET, algorithms=["HS256"])
         return payload  # contains user_id + role
-    except:
-        raise HTTPException(status_code=401, detail="Invalid token")
+    except ExpiredSignatureError as e:
+        raise HTTPException(status_code=401, detail="Token expired") from e
+    except InvalidSignatureError as e:
+        raise HTTPException(status_code=401, detail="Invalid token signature") from e
+    except DecodeError as e:
+        raise HTTPException(status_code=401, detail="Invalid token") from e
