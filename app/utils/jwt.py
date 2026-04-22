d6118737203ffc5cf4ee7adbf974c6e6e0c13f94
DiorHomme <chomunodisaa68@gmail.com>
2026-04-23 01:15:20 +0200

    Update jwt.py

--- app/utils/jwt.py
+++ app/utils/jwt.py
@@ -4,5 +4,6 @@ from app.config.settings import settings
 
 def create_token(data: dict):
     payload = data.copy()
-    payload["exp"] = datetime.utcnow() + timedelta(hours=2)
+    if settings.JWT_EXPIRES_HOURS > 0:
+        payload["exp"] = datetime.utcnow() + timedelta(hours=settings.JWT_EXPIRES_HOURS)
     return jwt.encode(payload, settings.JWT_SECRET, algorithm="HS256")
